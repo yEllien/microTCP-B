d0a1c438b14aa59dd323b1b7dd6c8f0659c23609
christine <christineb07@outlook.com.gr>
2021-12-02 19:02:52 +0200

    added return value in bind()

--- lib/microtcp.c
+++ lib/microtcp.c
@@ -37,26 +37,30 @@ microtcp_socket (int domain, int type, int protocol)
 int
 microtcp_bind (microtcp_sock_t *socket, const struct sockaddr *address,
                socklen_t address_len)
-{
-  if(bind(socket->sd, address, address_len) == -1){
+{ int rv;
+  if((rv = bind(socket->sd, address, address_len)) == -1){
     perror("TCP bind");
-    exit(0);
   }
   
+  return rv;
 }
 
 int
 microtcp_connect (microtcp_sock_t *socket, const struct sockaddr *address,
                   socklen_t address_len)
 {
-  /* Your code here */
+
 }
 
 int
 microtcp_accept (microtcp_sock_t *socket, struct sockaddr *address,
                  socklen_t address_len)
 {
-  /* Your code here */
+  microtcp_header_t *header;
+  struct sockaddr src_addr, src_addr_length;
+
+  socket->recvbuf = malloc(MICROTCP_RECVBUF_LEN * sizeof(char));
+  socket->buf_fill_level = recvfrom(socket->sd, &socket->recvbuf, MICROTCP_RECVBUF_LEN, MSG_WAITALL, &src_addr, &src_addr_len);
 }
 
 int
